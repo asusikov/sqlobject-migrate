01290b9b7c66c9e9666116e0e871bf7e03eb027f
asusikov <susikov.alexander@gmail.com>
2013-03-13 19:47:00 +0400

    #2 Add template. Generate file name.

--- sqlobjectmigrate/migrate.py
+++ sqlobjectmigrate/migrate.py
@@ -9,7 +9,7 @@ def up():
     from sqlobjectmigrate.version import Version
 
     projectDir = os.getcwd()
-    migrationDir = os.path.abspath(os.path.join(projectDir, "db"))
+    migrationDir = os.path.abspath(os.path.join(projectDir, 'db'))
     sys.path.append(projectDir)
     sys.path.append(migrationDir)
     import migration
@@ -41,34 +41,47 @@ def up():
 def down():
     print 'down'
 
-def generate():
+def generate(name):
     print 'generate'
+    from mako.template import Template
+    import time
+
+    number = time.strftime('%Y%m%d%H%M%S', time.gmtime())
+    
+    fileNameTemplate = Template('${number}_${name}.py')
+    print fileNameTemplate.render(number = number, name = name)
+    templateDir = os.path.split(os.path.abspath(__file__))[0]
+    templateName = os.path.join(templateDir, 'template.mako')
+    fileTemplate = Template(filename = templateName)
+    nameCapitalize = name[0].capitalize() + name[1:-1]
+    print fileTemplate.render(name = nameCapitalize)
 
 def main(args = sys.argv[1:]):
 
     parser = optparse.OptionParser()
-    parser.add_option("-g", "--generate",
-                      dest = "generate",
-                      action = "store_true",
+    parser.add_option('-g', '--generate',
+                      dest = 'generate',
+                      action = 'store_true',
                       default = False,
                       help = 'Run generator')
 
-    parser.add_option("-u", "--up",
-                      dest = "up",
-                      action = "store_true",
+    parser.add_option('-u', '--up',
+                      dest = 'up',
+                      action = 'store_true',
                       default = False,
                       help = 'Up database')
 
-    parser.add_option("-d", "--down",
-                      dest = "down",
-                      action = "store_true",
+    parser.add_option('-d', '--down',
+                      dest = 'down',
+                      action = 'store_true',
                       default = False,
                       help = 'Down database')
 
     options, args = parser.parse_args(args)
     
     if options.generate:
-        generate()
+        if len(args) > 0:
+            generate(args[0])
         return
     if options.up:
         up()
---
+++ sqlobjectmigrate/template.mako
@@ -0,0 +1,9 @@
+from sqlobjectmigrate.migrationBase import MigrationBase
+
+class ${name}(MigrationBase):
+
+    def up(self):
+        pass
+
+    def down(self):
+        pass
